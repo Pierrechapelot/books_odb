237c04b6fb0238492848af3e2311e6134ba7e742
Pierrechapelot <pierre.chapelot@gmail.com>
2023-08-20 18:17:30 +0200

    validation tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,22 @@
 class BooksController < ApplicationController
     def index
-        @books = Book.all 
+      @books = Book.all
     end
-
-
+  
     def show
-        @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
     end
-
-    def new 
-        @book= Book.new 
+  
+    def new
+      @book = Book.new
     end
-
+  
     def create
-        Book.create(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
+      @book = Book.new(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
+      if @book.save
         redirect_to books_path
+      else
+        render :new
+      end
     end
-end
+  end

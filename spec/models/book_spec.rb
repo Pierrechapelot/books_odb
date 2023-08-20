038f50a8761a3b965df2efa4ac2bffed04a6beab
Pierrechapelot <pierre.chapelot@gmail.com>
2023-08-20 11:05:11 +0200

    first commit

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+let(:book) {FactoryBot.build(:book)}
+
+  it "can be instanciated" do
+    expect(book).not_to be nil
+  end 
+  it "can be saved" do
+    expect(book.save).to be true
+  end
+end

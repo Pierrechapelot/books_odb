e9764dad6a25080f996dfd40403d10cf7c6780e1
Pierrechapelot <pierre.chapelot@gmail.com>
2023-08-20 11:36:09 +0200

    resource show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   get "home", to: "home#index"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :books, only: [:index, :show] 
 end

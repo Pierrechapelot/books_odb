e8ba1466026eed0bce1d775ca34ffe64f5a50a4f
Pierrechapelot <pierre.chapelot@gmail.com>
2023-08-21 16:57:10 +0200

    resource edition

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,44 @@
 class BooksController < ApplicationController
-    def index
-      @books = Book.all
-    end
-  
-    def show
-      @book = Book.find(params[:id])
-    end
-  
-    def new
-      @book = Book.new
-    end
-  
-    def create
-      @book = Book.new(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
-      if @book.save
-        redirect_to books_path
-      else
-        render :new
-      end
+  before_action :find_book, only: [:show, :edit, :update]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
     end
   end
+
+  def update
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :pages_count, :published_at)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
+    end
+end
